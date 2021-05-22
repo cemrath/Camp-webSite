9368754c9850d0c86c10006156d527092a7dc532
Curtis Martin <curtbusiness@gmail.com>
2021-05-22 17:18:22 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
 $(function() {
-    $(".carousel").carousel( { interval: 2000, pause: "false" } );
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
-        }
-    });
+$(".carousel").carousel( { interval: 2000, pause: "false" } );
+$("#carouselButton").on("click",carousel);
+$("#reserveButton").on("click", reserveModal);
+$("#loginButton").on("click", loginModal);
 });
+
+// Carousel Modal Play / Pause Button
+function carousel(){
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+        $(".carousel").carousel("pause");
+        $("#carouselButton").children("i").removeClass("fa-pause");
+        $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+        $(".carousel").carousel("cycle");
+        $("#carouselButton").children("i").removeClass("fa-play");
+        $("#carouselButton").children("i").addClass("fa-pause"); 
+    }
+}
+
+// Jumbotron Reserve Campsite Button
+function reserveModal(){
+    $("#reserveModal").modal('show');
+}
+
+// Login Modal Button
+function loginModal(){
+    $("#loginModal").modal('show');
+}
